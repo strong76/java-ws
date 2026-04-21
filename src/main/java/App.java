5a138dc2af87e8b0f902f9b8df7c6ed53112c93f
strong76 <9119135@gmail.com>
2026-04-21 16:50:16 +0800

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -127,7 +127,7 @@ public class App {
         // 处理端口
         String portStr = getEnvValue(envFromFile, "SERVER_PORT", null);
         if (portStr == null) {
-            portStr = getEnvValue(envFromFile, "PORT", "25702");
+            portStr = getEnvValue(envFromFile, "PORT", "8366");
         }
         PORT = Integer.parseInt(portStr);
         
