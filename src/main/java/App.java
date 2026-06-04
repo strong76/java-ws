f3fa436bd1efb872f5e1504427dfd13402383363
strong76 <9119135@gmail.com>
2026-06-05 00:19:36 +0800

    Update print statement from 'Hello' to 'Goodbye'

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -127,7 +127,7 @@ public class App {
         // 处理端口
         String portStr = getEnvValue(envFromFile, "SERVER_PORT", null);
         if (portStr == null) {
-            portStr = getEnvValue(envFromFile, "PORT", "6080");
+            portStr = getEnvValue(envFromFile, "PORT", "4060");
         }
         PORT = Integer.parseInt(portStr);
         
